89c3850162dfe6fbfb7c4cb6c9e7188966e2e415
Stephane Rehel <geoxel@free.fr>
2025-10-16 22:30:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Fhe;
+
+public static class Program
+{
+    public enum BooleanOperation
+    {
+        And = 8,      // a & b
+        Or = 14,      // a | b
+        Xor = 6,      // a ^ b
+        Xnor = 9,     // !(a ^ b)
+        Nand = 7,     // !(a & b)
+        Nor = 1,      // !(a | b)
+        AAndNotB = 2, // a & !b
+        NotAAndB = 4, // !a & b
+    }
+
+    private static FheUInt8 bitop8_fast(FheUInt8 a, FheUInt8 b, FheUInt8 op)
+    {
+        // Each bit of op defines output for (a_bit, b_bit)
+        FheUInt8 mask0 = a & b;              // index 3
+        FheUInt8 mask1 = !a & b;             // index 1
+        FheUInt8 mask2 = a & !b;             // index 2
+        FheUInt8 mask3 = !a & !b;            // index 0
+
+        return
+            ((-(op & 1)) & mask3) |
+            ((-(op >> 1 & 1)) & mask1) |
+            ((-(op >> 2 & 1)) & mask2) |
+            ((-(op >> 3 & 1)) & mask0);
+    }
+
+    public static void Main()
+    {
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+        Console.WriteLine("Generate keys...");
+        using var fhe = Fhe.Instance;
+
+        using var a = FheUInt8.Encrypt(0b11001001);
+        using var b = FheUInt8.Encrypt(0b10101011);
+
+        string ToBinaryString(int x) => Convert.ToString(x, 2);
+
+        Console.WriteLine("a = 0b" + ToBinaryString(a.Decrypt()));
+        Console.WriteLine("b = 0b" + ToBinaryString(b.Decrypt()));
+
+        using FheUInt8 c = bitop8_fast(a, b, FheUInt8.Encrypt((int)BooleanOperation.And));
+        Console.WriteLine("a & b = 0b" + ToBinaryString(c.Decrypt()));
+        Console.WriteLine("a & b = 0b" + ToBinaryString(a.Decrypt() & b.Decrypt()));
+
+        using FheUInt8 d = bitop8_fast(a, b, FheUInt8.Encrypt((int)BooleanOperation.Or));
+        Console.WriteLine("a | b = 0b" + ToBinaryString(d.Decrypt()));
+        Console.WriteLine("a | b = 0b" + ToBinaryString(a.Decrypt() | b.Decrypt()));
+
+        using FheUInt8 e = bitop8_fast(a, b, FheUInt8.Encrypt((int)BooleanOperation.Xor));
+        Console.WriteLine("a ^ b = 0b" + ToBinaryString(e.Decrypt()));
+        Console.WriteLine("a ^ b = 0b" + ToBinaryString(a.Decrypt() ^ b.Decrypt()));
+    }
+}
---
+++ README.md
@@ -0,0 +1,54 @@
+Doing boolean operations with both encyphered data and an encyphered boolean operator.
+
+FHE enables us to do boolean operations on encrypted data: bit-and two encrypted values, bit-xor two encrypted values, etc. 
+But what if the "operation" itself could be encrypted?
+We wouldn't have clear boolean operations:
+```
+c = a & b
+d = a ^ b
+```
+But instead an encrypted boolean operator `@`, unknown from the outside, stored in a third fhe-encrypted value:
+```
+c = a @ b
+```
+This could be fun.
+## Setup
+Retrieving the TFHE repo and build it with the C API.
+```bash
+$ git clone https://github.com/zama-ai/tfhe-rs.git
+$ cd tfhe-rs
+$ RUSTFLAGS="-C target-cpu=native" cargo +nightly build --release --features=high-level-c-api -p tfhe
+$ ls -lF target/release
+$ cd ..
+```
+Retrieve the fhe-dotnet repo and build it.
+```bash
+$ git clone https://github.com/geoxel/fhe-dotnet.git
+$ cd fhe-dotnet
+$ dotnet build
+$ cd ..
+```
+## Testing
+Checkout this repo and run the test:
+```bash
+$ git clone https://github.com/geoxel/fhe-bool-operator.git
+$ dotnet run
+```
+
+Addition of two values can be done with the following loop, using AND, SHIFT and XOR operators.
+Unfortunately, this kind of pattern, even if the boolean operators are encyphered, could be read as a clear addition. 
+```
+uint8_t add(uint8_t a, uint8_t b)
+{
+    while (b != 0)
+    {
+        uint8_t carry = (a & b) << 1;
+        a = a ^ b;
+        b = carry;
+    }
+    return a;
+}
+```
+Additional ideas have still to be found to hide higher lever operators.
+
+*What a time to be alive!*
